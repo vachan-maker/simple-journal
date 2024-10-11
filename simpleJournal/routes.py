071110936d8ff47057cf12d983096f4d6069c540
vachan-maker <vachan@tutanota.com>
2024-10-11 21:51:07 +0530

    added exception handling

--- simpleJournal/routes.py
+++ simpleJournal/routes.py
@@ -1,15 +1,19 @@
-from flask import render_template,url_for,redirect,request
+from flask import render_template,url_for,redirect,request,flash
 from simpleJournal import app, db
 from simpleJournal.model import JournalEntry
+from sqlalchemy.exc import SQLAlchemyError, DBAPIError
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/entry', methods=["POST"])
 def entry():
-    if request.method == "POST":
-        newEntry = JournalEntry(id=100,title=str(request.form["title"]),textEntry=str(request.form["text"]))
+    try:
+        newEntry = JournalEntry(title=str(request.form["title"]),textEntry=str(request.form["text"]))
         db.session.add(newEntry)
         db.session.commit()
-    return render_template('index.html')
+    except(SQLAlchemyError, DBAPIError) as e:
+        return e
+
+    return redirect(url_for('index'))
 
