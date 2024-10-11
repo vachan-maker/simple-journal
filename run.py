19e95afeb280dd42cc2a8442c23f15151bcc3cc9
vachan-maker <vachan@tutanota.com>
2024-10-11 20:00:14 +0530

    added flowbite and tailwind

---
+++ run.py
@@ -0,0 +1,4 @@
+#running the application
+from simpleJournal import app
+if __name__ == '__main__':
+    app.run(debug=True)
--- simpleJournal/routes.py
+++ simpleJournal/routes.py
@@ -1,2 +1,6 @@
 from flask import render_template,url_for,redirect
 from simpleJournal import app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
