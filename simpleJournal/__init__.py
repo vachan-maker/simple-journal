6f3f479ebf82995a0603af6421492ada9256893d
vachan-maker <vachan@tutanota.com>
2024-10-12 18:37:00 +0530

    getting ready for deploymeny

--- simpleJournal/__init__.py
+++ simpleJournal/__init__.py
@@ -7,7 +7,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///journal.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 db = SQLAlchemy(app)
 #creating instance of the database
